3fb9f25439ddb60eac6efd4b8e4cd35f4185e780
mlaszewski <laszewskim18@gmail.com>
2023-11-26 12:10:56 +0100

    API init and Movie model created

---
+++ MyMoviesAPI/Models/Movie.cs
@@ -0,0 +1,10 @@
+﻿namespace MyMoviesAPI.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public int YearOfRelease { get; set; }
+        public string Director { get; set; } = string.Empty;
+    }
+}
