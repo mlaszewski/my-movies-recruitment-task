8b05bbcc6998238d1ff23def93af4fe174ed7638
mlaszewski <laszewskim18@gmail.com>
2023-11-29 00:23:01 +0100

    CORS Fix

--- my-movies-client/src/main.js
+++ my-movies-client/src/main.js
@@ -2,13 +2,14 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import 'bootstrap/dist/css/bootstrap.css';
 import axios from 'axios';
+import VueAxios from 'vue-axios'
 import VueSweetalert2 from 'vue-sweetalert2';
 import 'sweetalert2/dist/sweetalert2.min.css';
 
-axios.defaults.baseURL = process.env.VUE_APP_BASEURL;
-
 const app = createApp(App);
 
-app.use(VueSweetalert2);
+axios.defaults.baseURL = process.env.VUE_APP_BASEURL;
 
+app.use(VueSweetalert2);
+app.use(VueAxios, axios);
 app.mount('#app');
--- my-movies-client/vue.config.js
+++ my-movies-client/vue.config.js
@@ -2,12 +2,7 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   devServer: {
-    proxy: {
-      "api": {
-        target: process.env.VUE_APP_BASEURL,
-        ws: true,
-        changeOrigin: true
-      }
-    }
+    port: 3000,
+    proxy: "https://localhost/"
   }
 })
